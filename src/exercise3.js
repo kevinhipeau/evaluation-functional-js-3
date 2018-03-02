04304e40ea95549dfc16455fa41337c6dda43b18
Kévin <kevin.hipeau@gmail.com>
2018-03-02 10:52:08 +0100

    exo 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,20 +1,15 @@
 const _ = require("lodash");
 
 const meeteek = input => {
-  let tmp = input.reduce((acc, perso) => {
+  return input.reduce((acc, perso) => {
     acc[perso.firstname] = input.filter(findp => _.intersection(findp.interests, perso.interests).length > 0 && perso.sexPartnerApproved.includes(findp.sex) && perso.firstname != findp.firstname)
       .reduce((accp, persook) => {
-
         accp.push({ firstname: persook.firstname, commonInterests: _.intersection(persook.interests, perso.interests) })
-
         return accp;
       }, []);
 
     return acc;
   }, {})
-
-  console.log(tmp)
-  return tmp;
 };
 
 module.exports = {
