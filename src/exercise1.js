fddfd2d15d6ac609fb00de49d5c6140f72c48d56
Kévin <kevin.hipeau@gmail.com>
2018-03-02 09:41:38 +0100

    Exo 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,7 +1,11 @@
 const moment = require("moment");
 
 const sortProduct = input => {
-  // Your future job begins here ...
+  return input.filter(p => moment(p.dateAdded).year() > 2000).sort(p => !p.dateAdded).reduce((acc, p) => {
+    var tmp = moment(p.dateAdded);
+    acc[p.name] = [{ name: p.name, year: tmp.year(), monthOfYear: tmp.month() + 1, quantity: p.quantity }];
+    return acc;
+  }, {});
 };
 
 module.exports = {
